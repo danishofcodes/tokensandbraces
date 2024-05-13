6e64d10d76c007a8e55930fd76b87bdd5a35e502
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-05-13 19:11:09 +0530

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,31 +1,19 @@
-import reactIcon from '../assets/react.svg'
-// import { useRef, useEffect } from 'react';
-export default function ProjectCard() {
-    // const cardRef = useRef(null);
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 
-    // useEffect(() => {
-    //     const observer = new IntersectionObserver((entries) => {
-    //         const [entry] = entries;
-    //         if (entry.isIntersecting) {
-    //             cardRef.current.classList.add('slide-in');
-    //         }
-    //     });
-    
-    //     observer.observe(cardRef.current);
-    
-    //     return () => observer.unobserve(cardRef.current);
-    // }, []); // Empty dependency array to run this effect only once
-
+export default function ProjectCard({title, desc, img , link}) {
 
     return (
         <div className="card rise">
             <div className="card-image">
-                <img src={reactIcon} alt="Image description" />
+                <img src={img} alt="Image description" />
             </div>
-            <div className="card-details">
-                <h3 className='font-bold text-base'>Project Title</h3>
-                <p className='mb-3'>This is a brief description of your GitHub project. You can highlight key features or functionalities here.</p>
-                <a href="https://github.com/your-username/your-repo" >View on GitHub</a>
+            <div className="card-details flex">
+                <div>
+                <h3 className='font-bold text-base'>{title}</h3>
+                <p className='mb-1'>{desc}</p>
+                </div>
+                <a href={link} ><FontAwesomeIcon icon={faCode} /> View on GitHub </a>
             </div>
         </div>
     )
