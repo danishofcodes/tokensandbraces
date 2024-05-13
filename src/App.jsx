fc35fc20f589f24256c4939efad4876772fb7926
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-05-13 19:10:47 +0530

    projects  details array added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import MainSection from './components/MainSection'
+import reactIcon from './assets/react.svg'
 import Navbar from './components/Navbar'
 function App() {
+ 
+  const projects = [
+    {
+      projectname: 'GameWyre ',
+      description: 'GameWyre An E-commerce Platform for Gaming Products (Made with React JS), with filters , easy search and add to cart for quick checkout ',
+      imgKey: reactIcon, // Replace with your image file name or URL
+      githublink : 'https://github.com/danishofcodes/GameWyre'
+    },
+    {
+      projectname: 'Spotify-Clone',
+      description: 'Spotify Clone Made with Pure HTML5 CSS3. Thinking about rebuilding this in React afterwards.',
+      imgKey: reactIcon, // Replace with your image file name or URL
+      githublink : 'https://github.com/danishofcodes/Spotify-Clone'
+
+    },
+
+    {
+      projectname: 'MetroCity-AQI',
+      description: 'Visualize Air Quality Index of 6 major Indian cities, Compare AQI, calculate yearly averages. Interactive graphs for Insightful analysis.',
+      imgKey: reactIcon, // Replace with your image file name or URL
+      githublink : 'https://github.com/danishofcodes/metrocityAQI'
+    },
+
+  ];
+
+
   return (
     <>
     <Navbar/>
-     <MainSection/> 
+     <MainSection projects={projects}/> 
      </>
   )
 }
