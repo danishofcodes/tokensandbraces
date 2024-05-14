374d24dafefb01a5b143b1baee23d9ad6830f2bd
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-05-14 18:40:21 +0530

    Nav, added overlayed fullscreen menu for mobile devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,10 +15,10 @@ export default function Navbar() {
         Danish's Folio
       </div>
       <div className="hidden  lg:flex items-center space-x-4">
-        <a href="#" className="text-slate-700 dark:text-slate-200 text-sm leading-6 font-semibold lg:flex items-center hover:text-[#801DFF] "><FontAwesomeIcon icon={faGithub} style={{ fontSize: '30px' }}/></a>
+        <a href="https://github.com/danishofcodes" className="text-slate-700 dark:text-slate-200 text-sm leading-6 font-semibold lg:flex items-center hover:text-[#801DFF] "><FontAwesomeIcon icon={faGithub} style={{ fontSize: '30px' }}/></a>
         {/* <a href="#" className="text-slate-700 dark:text-slate-200 text-sm leading-6 font-semibold">Option 2</a>  bg-stone-200 px-2 py-2 rounded border-b-2 border-fuchsia-600*/}
       
-        <a href="#" className="text-slate-700 dark:text-slate-200  leading-6 font-semibold flex text-xl hover:text-[#801DFF] "><FontAwesomeIcon icon={faEnvelope} className='' style={{ fontSize: '25px' }} /></a>
+        <a href="mailto:someone@something.com" className="text-slate-700 dark:text-slate-200  leading-6 font-semibold flex text-xl hover:text-[#801DFF] "><FontAwesomeIcon icon={faEnvelope} className='' style={{ fontSize: '25px' }} /></a>
 
       </div>
       <div className="lg:hidden" onClick={openCollapsibleNav}>
@@ -26,9 +26,9 @@ export default function Navbar() {
       </div>
       <div className={visible ?'overlayNav' : 'hidden'}>
         <div className='flex justify-end items-center p-3'> 
-        <button className="h-10 w-10" onClick={openCollapsibleNav}><FontAwesomeIcon icon={faClose} style={{fontSize:"25px"}}/></button>
+        <button className="h-10 w-10" onClick={openCollapsibleNav}><FontAwesomeIcon icon={faClose} style={{fontSize:"35px"}}/></button>
         </div>
-         <div className='px-4 py-2 optSocial'><FontAwesomeIcon icon={faGithub} style={{fontSize:"25px"}} className='me-2'/>Github</div> 
+         <div className='px-4 py-2 optSocial'><FontAwesomeIcon icon={faGithub} style={{fontSize:"30px"}} className='me-2'/>Github</div> 
          <hr/>
          <div className='px-4 py-2 optSocial'><FontAwesomeIcon icon={faEnvelope} style={{fontSize:"25px"}} className='me-2'/>Email</div>  
          <hr/>
