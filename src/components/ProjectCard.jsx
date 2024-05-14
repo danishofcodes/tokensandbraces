6d5530fe1f881698bed77964042d0e74bcbf01e1
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-05-14 18:38:51 +0530

    Renders Project Card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { faChrome } from "@fortawesome/free-brands-svg-icons";
 
 export default function ProjectCard({title, desc, img , link}) {
 
@@ -13,7 +14,10 @@ export default function ProjectCard({title, desc, img , link}) {
                 <h3 className='font-bold text-base'>{title}</h3>
                 <p className='mb-1'>{desc}</p>
                 </div>
-                <a href={link} ><FontAwesomeIcon icon={faCode} /> View on GitHub </a>
+                <div className="flex items-center justify-evenly">
+                <a href={link} className=" bg-stone-100 text-stone-600 px-3 py-1 rounded-full hover:bg-stone-400" ><FontAwesomeIcon icon={faChrome} /> View</a>
+                <a href={link} className="px-3"><FontAwesomeIcon icon={faCode} /> Source Code </a>
+                </div>
             </div>
         </div>
     )
