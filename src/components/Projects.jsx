54a55d6e80d5a795761a81fb2d894772d0d03b55
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-05-15 18:45:01 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ export default function Projects({projects}) {
 {/* {projectsVisible ? "yes" : "no"} */}
 <div className='flex flex-col'>
     {projects.map((item, index) => (
-        <ProjectCard key={index} title={item.projectname} desc={item.description} img={item.imgKey} link={item.githublink}/> // Pass an index as a key for each iteration
+        <ProjectCard key={index} title={item.projectname} desc={item.description} img={item.imgKey} link={item.githublink} view={item.viewonbrowser}/> // Pass an index as a key for each iteration
     ))}
 </div>
 </div>
