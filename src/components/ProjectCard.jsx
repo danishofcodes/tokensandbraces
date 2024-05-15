4b972b8bc98ee12f6e4cd277b290d44249946315
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-05-15 18:44:54 +0530

    link for view on browser added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCode } from '@fortawesome/free-solid-svg-icons';
 import { faChrome } from "@fortawesome/free-brands-svg-icons";
 
-export default function ProjectCard({title, desc, img , link}) {
+export default function ProjectCard({title, desc, img , link, view}) {
 
     return (
         <div className="card rise">
@@ -15,7 +15,7 @@ export default function ProjectCard({title, desc, img , link}) {
                 <p className='mb-1'>{desc}</p>
                 </div>
                 <div className="flex items-center justify-evenly">
-                <a href={link} className=" bg-stone-100 text-stone-600 px-3 py-1 rounded-full hover:bg-stone-400" ><FontAwesomeIcon icon={faChrome} /> View</a>
+                <a href={view} className=" bg-stone-100 text-stone-600 px-3 py-1 rounded-full hover:bg-stone-400 me-1" ><FontAwesomeIcon icon={faChrome} /> View </a>
                 <a href={link} className="px-3"><FontAwesomeIcon icon={faCode} /> Source Code </a>
                 </div>
             </div>
