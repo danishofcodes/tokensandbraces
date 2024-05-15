40c6d1ebef5a88cf767ce6cacbf3fcf4863b7877
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-05-15 18:44:35 +0530

    Update PinnedBlog.jsx

--- src/components/PinnedBlog.jsx
+++ src/components/PinnedBlog.jsx
@@ -7,7 +7,7 @@ export default function PinnedBlog({blogs}) {
         <h4 className='font-bold text-3xl mb-3'>Pinned Blogs And Videos</h4>
         <div className='flex gap-5 flex-wrap justify-between'>
         {blogs.map((item, index) => (
-        <BlogCard key={index} title={item.title} desc={item.description} img={item.imgKey} link={item.bloglink}/> // Pass an index as a key for each iteration
+        <BlogCard key={index} title={item.title} desc={item.description} img={item.imgKey} link={item.hyperlink} isVideo={item.isVideo} sitename={item.sitename}/> // Pass an index as a key for each iteration
     ))}
     </div>
     </div>
