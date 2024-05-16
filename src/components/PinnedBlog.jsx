900880d4170637ae2dbd44bd1dcf01c9a4d1512d
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-05-16 18:34:52 +0530

    Update PinnedBlog.jsx

--- src/components/PinnedBlog.jsx
+++ src/components/PinnedBlog.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import BlogCard from './BlogCard'
 
-export default function PinnedBlog({blogs}) {
+export default function PinnedBlog({ blogs }) {
   return (
     <div>
-        <h4 className='font-bold text-3xl mb-3'>Pinned Blogs And Videos</h4>
-        <div className='flex gap-5 flex-wrap justify-between'>
+      <h4 className='font-bold text-3xl mb-3'>Pinned Blogs And Videos</h4>
+      <div className='flex gap-5 flex-wrap'>
         {blogs.map((item, index) => (
-        <BlogCard key={index} title={item.title} desc={item.description} img={item.imgKey} link={item.hyperlink} isVideo={item.isVideo} sitename={item.sitename}/> // Pass an index as a key for each iteration
-    ))}
-    </div>
+          <BlogCard key={index} title={item.title} desc={item.description} img={item.imgKey} link={item.hyperlink} isVideo={item.isVideo} sitename={item.sitename} /> // Pass an index as a key for each iteration
+        ))}
+      </div>
     </div>
   )
 }
