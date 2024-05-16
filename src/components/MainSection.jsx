82e00f3a2167e7ffe3315df888c835b61b64df96
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-05-16 18:35:49 +0530

    Update MainSection.jsx

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -66,37 +66,42 @@ export default function MainSection({ projects, blogs }) {
     return (
         <main className="main-section pt-5">
             <div className="left-part">
+                {/* <img src={} alt="" srcset="" /> */}
+                <div className='terminalgif'>
+                    <div className='terminaltype'></div>
+                </div>
 
-               
                 <h1 className="text-4xl font-bold tracking-tight text-[#3b3b3b] sm:text-5xl"><a href="/">Tom Cat</a></h1>
                 <h2 className="mt-3 text-lg  tracking-tight sm:text-xl text-green-500 font-bold">&#123;Web Developer&#125; <FontAwesomeIcon icon={faCode} /></h2>
                 {/* <div className='terminalgif'><img src={terminalgif} alt="" srcset="" /></div> */}
                 <p className="mt-4 max-w-xs leading-normal">I build pixel-perfect, engaging, and accessible digital experiences.</p>
-          
+
 
                 <div className='navPoints mt-3' >
-                    
-                    <ul className="lg:h-48 h-auto">
-                    {/* ${aboutMeVisible ? 'active' : ''}` */}
-                    {/* ${projectsVisible ? 'active' : ''}` className="font-bold text-base" */}
-                    <Link to="/">  <li className={location.pathname === "/" ? "active" : "font-bold text-base"} ><FontAwesomeIcon icon={faQuoteLeft} className='me-2' /><a href="#allaboutme">About</a></li></Link>
-                    <Link to="/Projects">    <li className={location.pathname === "/Projects" ? "active" : "font-bold text-base"} ><FontAwesomeIcon icon={faCodeFork} className='me-2' /><a href="#projectsSec">My Projects</a></li></Link>
-                    <Link to="/Ongoing"><li className={location.pathname === "/Ongoing" ? "active" : "font-bold text-base"} ><FontAwesomeIcon icon={faSpinner} className='me-2' />Ongoing</li></Link>
-                    <Link to="/PinnedBlogs"><li className={location.pathname === "/PinnedBlogs" ? "active" : "font-bold text-base"} ><FontAwesomeIcon icon={faThumbTack} className='me-2' />Pinned Blogs</li></Link>
+
+                    <ul className="lg:h-48 h-auto ">
+                        {/* ${aboutMeVisible ? 'active' : ''}` */}
+                        {/* ${projectsVisible ? 'active' : ''}` className="font-bold text-base" */}
+                        <Link to="/">  <li className={location.pathname === "/" ? "active" : "font-bold text-base"} ><FontAwesomeIcon icon={faQuoteLeft} className='me-2' /><a href="#allaboutme">About</a></li></Link>
+                        <Link to="/Projects">    <li className={location.pathname === "/Projects" ? "active" : "font-bold text-base"} ><FontAwesomeIcon icon={faCodeFork} className='me-2' /><a href="#projectsSec">My Projects</a></li></Link>
+                        <Link to="/Ongoing"><li className={location.pathname === "/Ongoing" ? "active" : "font-bold text-base"} ><FontAwesomeIcon icon={faSpinner} className='me-2' />Ongoing</li></Link>
+                        <Link to="/PinnedBlogs"><li className={location.pathname === "/PinnedBlogs" ? "active" : "font-bold text-base"} ><FontAwesomeIcon icon={faThumbTack} className='me-2' />Pinned Blogs</li></Link>
                     </ul>
 
-                    <a href="https://drive.google.com/file/d/1HgTNTIi1jJf6Z7nIQOZYKQfbNIqrLPqf/view?usp=drive_link"> <button className="bg-transparent hover:bg-green-500 text-indigo-600 font-semibold hover:text-white py-2 px-4 border border-indigo-600 hover:border-transparent rounded flex ">
-                        <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
-                        <span>Download CV</span>
-                    </button></a>
+                    <div className="mt-1" >
+                        <a target="_blank" href="https://drive.google.com/file/d/1HgTNTIi1jJf6Z7nIQOZYKQfbNIqrLPqf/view?usp=drive_link"> <button className="bg-transparent hover:bg-green-500 text-indigo-600 font-semibold hover:text-white py-2 px-4 border border-indigo-600 hover:border-transparent rounded flex ">
+                            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+                            <span>Download CV</span>
+                        </button></a>
+                    </div>
                 </div>
             </div>
-            <div className="right-part  overflow-y-auto h-4/5">
+            <div className="right-part ">
                 <Routes>
                     <Route path="/" element={<Info />} />
                     <Route path="/Projects" element={<Projects projects={projects} />} />
                     <Route path="/Ongoing" element={<Ongoing />} />
-                    <Route path="/PinnedBlogs" element={<PinnedBlog blogs={blogs}/>} />
+                    <Route path="/PinnedBlogs" element={<PinnedBlog blogs={blogs} />} />
                 </Routes>
             </div>
         </main>
