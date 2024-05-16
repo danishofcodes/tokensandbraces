10b95c46baedc21710429b60f5bb9f07d6190db5
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-05-16 18:35:16 +0530

    Update Ongoing.jsx

--- src/components/Ongoing.jsx
+++ src/components/Ongoing.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Ongoing() {
   return (
     <div>
-    <h4 className='font-bold text-3xl mb-3'>Ongoing Projects</h4>
-        <ul>
-            <li>project 1</li>
-            <li>proj 2</li>
-            <li>project 3</li>
-        </ul>
+      <h4 className='font-bold text-3xl mb-3'>Ongoing Projects</h4>
+      <ul>
+        <li>project 1</li>
+        <li>proj 2</li>
+        <li>project 3</li>
+      </ul>
     </div>
   )
 }
