a6944c01be73abfd6023121007dbd641b8f60680
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-06-14 18:33:19 +0530

    Update projectsdata.js

--- src/data/projectsdata.js
+++ src/data/projectsdata.js
@@ -3,6 +3,7 @@ import reactIcon from '../assets/react.svg'
 import { faGamepad } from '@fortawesome/free-solid-svg-icons/faGamepad';
 import { faCalendarCheck, faCode, faKey, faListCheck, faLock, faPoll, faWind } from '@fortawesome/free-solid-svg-icons';
 export const projects = [
+  
     {
       projectname: 'GameWyre ',
       description: 'GameWyre An E-commerce Platform for Gaming Products (Made with React JS), with filters , easy search and add to cart for quick checkout ',
@@ -10,9 +11,18 @@ export const projects = [
       githublink: 'https://github.com/danishofcodes/GamerWyre',
       viewonbrowser: 'https://danishofcodes.github.io/GamerWyre/'
     },
+    
+    {
+      projectname: 'SurveyBubble : User Feedback Surveyor',
+      description: 'Feedback Surveyor: Create and manage feedback surveys, Made with React JS And TailwindCSS. ',
+      imgKey: faPoll, // Replace with your image file name or URL
+      githublink: 'https://github.com/danishofcodes/metrocityAQI',
+      viewonbrowser: 'https://danishofcodes.github.io/metrocityAQI/'
+    },
+
     {
       projectname: 'Forms And Validations ',
-      description: 'Various Forms and Their Data Validations using Formik and Yup Library and Custom Validation.',
+      description: 'Signup form, Newsletter Form, Personal Data Form and Their Data Input and Validations Made With React JS, Formik, YUP',
       imgKey: faListCheck, // Replace with your image file name or URL
       githublink: 'https://github.com/danishofcodes/FormsAndValidation',
       viewonbrowser: 'https://danishofcodes.github.io/FormsAndValidation/'
@@ -20,16 +30,8 @@ export const projects = [
     },
 
     {
-      projectname: 'User Feedback Surveyor',
-      description: 'Feedback Surveyor: A React app for effortless creation and distribution of feedback surveys.',
-      imgKey: faPoll, // Replace with your image file name or URL
-      githublink: 'https://github.com/danishofcodes/metrocityAQI',
-      viewonbrowser: 'https://danishofcodes.github.io/metrocityAQI/'
-    },
-
-    {
       projectname: 'Crypt My Texts',
-      description: 'Secure Your Messages: Encrypt & Decrypt Text easily with the help of "CryptMyTexts" helps you encrypt and decrypt texts, paragraphs or messages!',
+      description: 'Secure Your Messages: Encrypt & Decrypt Text easily with the help of "CryptMyTexts" helps you encrypt and decrypt texts, paragraphs or messages! Made With React JS and CryptJS',
       imgKey: faKey, // Replace with your image file name or URL
       githublink: 'https://github.com/danishofcodes/CryptMyTexts',
       viewonbrowser: 'https://danishofcodes.github.io/CryptMyTexts/'
@@ -37,14 +39,14 @@ export const projects = [
 
     {
       projectname: 'MetroCity-AQI',
-      description: 'Visualize Air Quality Index of 6 major Indian cities, Compare AQI, calculate yearly averages. Interactive graphs for Insightful analysis.',
+      description: 'Visualize Air Quality Index of 6 major Indian cities, Compare AQI, calculate yearly averages. Interactive graphs for Insightful analysis. Made with Pure JS and Chart JS',
       imgKey: faWind, // Replace with your image file name or URL
       githublink: 'https://github.com/danishofcodes/metrocityAQI',
       viewonbrowser: 'https://danishofcodes.github.io/metrocityAQI/'
     },
     {
         projectname: 'My To Do App',
-        description: 'A sleek looking useful todo app to keep note of your daily task ! :]',
+        description: 'A sleek looking useful todo app to keep note of your daily task ! :] Made with ReactJS And TailwindCSS',
         imgKey: faCalendarCheck, // Replace with your image file name or URL
         githublink: 'https://github.com/danishofcodes/myTodoApp',
         viewonbrowser: 'https://danishofcodes.github.io/myTodoApp/'
