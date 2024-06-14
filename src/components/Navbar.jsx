52c70b88169f6d5dfad403e3e02cdd7a9b5b7342
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-06-14 18:33:02 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,11 +16,11 @@ export default function Navbar() {
        <FontAwesomeIcon icon={faUserAstronaut} className='text-2xl '/> Welcome to my corner of the web!
       </div>
       <div className="hidden  lg:flex items-center space-x-4">
-        <a href="https://github.com/danishofcodes" className="text-slate-700 dark:text-slate-200 text-sm leading-6 font-semibold lg:flex items-center hover:text-[#801DFF] "><FontAwesomeIcon icon={faGithub} style={{ fontSize: '25px' }} /></a>
+        <a href="https://github.com/danishofcodes" target="_blank" className="text-slate-700 dark:text-slate-200 text-sm leading-6 font-semibold lg:flex items-center hover:text-[#801DFF] "><FontAwesomeIcon icon={faGithub} style={{ fontSize: '25px' }} /></a>
         {/* <a href="#" className="text-slate-700 dark:text-slate-200 text-sm leading-6 font-semibold">Option 2</a>  bg-stone-200 px-2 py-2 rounded border-b-2 border-fuchsia-600*/}
-        <a href="linkedin.co.in" className="text-slate-700 dark:text-slate-200  leading-6 font-semibold flex text-xl hover:text-[#801DFF] "><FontAwesomeIcon icon={faLinkedin} className='' style={{ fontSize: '25px' }} /></a>
+        <a href="https://www.linkedin.com/in/abdul-danish-38b5942a7" target="_blank" className="text-slate-700 dark:text-slate-200  leading-6 font-semibold flex text-xl hover:text-[#801DFF] "><FontAwesomeIcon icon={faLinkedin} className='' style={{ fontSize: '25px' }} /></a>
 
-        <a href="mailto:someone@something.com" className="text-slate-700 dark:text-slate-200  leading-6 font-semibold flex text-xl hover:text-[#801DFF] "><FontAwesomeIcon icon={faEnvelope} className='' style={{ fontSize: '25px' }} /></a>
+        <a href="mailto:heyabduldanish@gmail.com" target="_blank" className="text-slate-700 dark:text-slate-200  leading-6 font-semibold flex text-xl hover:text-[#801DFF] "><FontAwesomeIcon icon={faEnvelope} className='' style={{ fontSize: '25px' }} /></a>
 
       </div>
       <div className="lg:hidden" onClick={openCollapsibleNav}>
