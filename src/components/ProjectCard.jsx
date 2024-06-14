244ec654bc862e51ddf36d46023e51ee4fc13f74
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-06-14 18:33:07 +0530

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -16,7 +16,7 @@ export default function ProjectCard({ title, desc, img, link, view , bgColor}) {
                     <p className='mb-1'>{desc}</p>
                 </div>
                 <div className="flex items-center justify-evenly">
-                    <a href={view} target="_blank" className=" bg-[#ff8b36] text-stone-100 px-3 py-1 rounded-full hover:bg-[#222d74] hover:text-white  me-1" ><FontAwesomeIcon icon={faChrome} /> View </a>
+                    <a href={view} target="_blank" className=" bg-[#ff8b36] text-stone-100 px-3 py-1 rounded-full hover:bg-[#801dff] hover:text-white  me-1" ><FontAwesomeIcon icon={faChrome} /> View </a>
                     <a href={link} target="_blank" className="px-3 hover:text-[#22c55e]"><FontAwesomeIcon icon={faCode} /> Source Code </a>
                 </div>
             </div>
