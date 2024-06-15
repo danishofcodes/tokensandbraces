1133e88066a807e3a593fa57ce408b6b719b3081
danish <danishofcodes@gmail.com>
2024-06-15 16:49:12 +0530

    Added deployment scripts and build for GitHub Pages

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tokens and braces : my portfolio site where I keep my best work and words
+
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,88 +1,30 @@
 import React from 'react'
 import { useRef, useEffect, useState } from 'react';
-// import { useInView } from 'react-intersection-observer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbTack } from '@fortawesome/free-solid-svg-icons';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { faHashtag } from '@fortawesome/free-solid-svg-icons';
 import { faCodeFork } from '@fortawesome/free-solid-svg-icons';
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
 import { faCode } from '@fortawesome/free-solid-svg-icons';
-import { Routes, Route, Link } from 'react-router-dom';
-import Ongoing from './Ongoing';
+import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import Pinned from './Pinned';
 import Info from './Info';
 import Projects from './Projects';
-import { useLocation } from 'react-router-dom';
-import terminalgif from '../assets/record-hello.gif'
 export default function MainSection({ projects, posts }) {
     const location = useLocation();
-    //   const aboutMeRef = useRef();
-    //   const experienceRef = useRef();
-    //   const projectsRef = useRef();
-    //  const [projectsVisible , setprojectsVisible] = useState('');
-    //  const [experienceVisible , setexperienceVisible] = useState('');
-
-    // const experienceRef = useRef(null);
-    // const projectsRef = useRef(null);
-
-    //  const { ref : aboutMeRef, inView : elemVisible } = useInView();
-    //  const { ref : experienceRef, inView : elemVisible } = useInView();
-    // const { ref: experienceRef, inView: aboutMeVisible } = useInView({ threshold: 1 });
-    // const { ref: projectsRef, inView: projectsVisible } = useInView({ threshold: 1 });
-
-
-
-
-    //    function handleScollToVIew(itemId){
-    //     if(itemId = 1){
-    //         experienceRef.current.scrollIntoView({ behavior: "smooth" });
-
-    //     }
-    //     else if(itemId = 2){
-    //     projectsRef.current.scrollIntoView({ behavior: "smooth" });
-
-    // }
-    //     };
-
-
-
-    // const projects = Array(4).fill(null); // Create an array with 8 null elements
-    // console.log('entry', elemVisible);
-
-    // useEffect(()=>{
-    // //   console.log('about Me Ref', aboutMeRef.current)
-    // //   console.log('Exp Ref', experienceRef.current)
-    // //   console.log('Projects Ref', projectsRef.current)
-
-    //   const observer = new IntersectionObserver((entries)=>{
-    //     const entry = entries[0];
-    //     setElemVisible(entry.isIntersecting)
-    //   })
-
-    //   observer.observe(projectsRef.current)
-
-    // }, [])
-
     return (
         <main className="main-section pt-5">
             <div className="left-part">
-                {/* <img src={} alt="" srcset="" /> */}
                 <div className='terminalgif'>
                     <div className='terminaltype'></div>
                 </div>
 
                 <h1 className="text-4xl font-bold tracking-tight text-[#3b3b3b] sm:text-5xl"><a href="/">Hi, I am Danish 👋</a></h1>
                 <h2 className="mt-3 text-lg  tracking-tight sm:text-xl text-green-500 font-bold">&#123;Web Developer&#125; <FontAwesomeIcon icon={faCode} /></h2>
-                {/* <div className='terminalgif'><img src={terminalgif} alt="" srcset="" /></div> */}
                 <p className="mt-4 max-w-xs leading-normal">I build pixel-perfect, engaging, and accessible digital experiences.</p>
 
 
                 <div className='navPoints mt-3' >
-
                     <ul className="lg:h-48 h-auto ">
-                        {/* ${aboutMeVisible ? 'active' : ''}` */}
-                        {/* ${projectsVisible ? 'active' : ''}` className="font-bold text-base" */}
                         <Link to="/" >  <li className={location.pathname === "/" ? "active" : "font-bold text-base"} ><FontAwesomeIcon icon={faQuoteLeft} className='me-2' />About</li></Link>
                         <Link to="/Projects">    <li className={location.pathname === "/Projects" ? "active" : "font-bold text-base"} ><FontAwesomeIcon icon={faCodeFork} className='me-2' />My Projects</li></Link>
                         {/* <Link to="/Ongoing"><li className={location.pathname === "/Ongoing" ? "active" : "font-bold text-base"} ><FontAwesomeIcon icon={faSpinner} className='me-2' />Ongoing</li></Link> */}
