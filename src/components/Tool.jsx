798c7b02abe719c3577eefdf67ce3a3c0e112844
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-06-20 11:33:47 +0530

    Update Tool.jsx

--- src/components/Tool.jsx
+++ src/components/Tool.jsx
@@ -2,7 +2,7 @@ export default function Tool({ tool }) {
   return (
     <div className="toolofchoice-card shrink-0">
       <span className="iconspace">
-        <img src={tool.icon} />
+        <img src={tool.icon} alt={tool.name} />
       </span>
       &nbsp;<span className="text-base">{tool.name}</span>
     </div>
