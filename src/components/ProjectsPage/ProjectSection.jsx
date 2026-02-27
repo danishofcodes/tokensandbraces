76c27528760d3f2f94175c342f975d484c35a6f3
danish <danishofcodes@gmail.com>
2026-02-27 20:08:41 +0530

    changes subheads

--- src/components/ProjectsPage/ProjectSection.jsx
+++ src/components/ProjectsPage/ProjectSection.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 
-const ProjectSection = ({ title, projects }) => {
+const ProjectSection = ({ title, subheading , projects }) => {
   const isFeatured = projects.some(p => p.featured);
 
   return (
     <section className="max-w-7xl mx-auto px-6 py-8">
       <h2 className="text-3xl font-bold mb-2">{title}</h2>
       <p className="text-gray-500 mb-10">
-        A selection of things I’ve built — from experiments to production-ready apps.
+       {subheading}
       </p>
 
       <div className={isFeatured ? "grid md:grid-cols-2 gap-8" : "grid md:grid-cols-3 gap-4"}>
